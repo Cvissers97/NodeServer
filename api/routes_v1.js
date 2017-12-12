d747e15d1c772553ef64d5c5df6a3259326fea1f
Casper // Astral <Cvissers97@users.noreply.github.com>
2017-12-12 15:34:36 +0100

    modified routes file

--- api/routes_v1.js
+++ api/routes_v1.js
@@ -1,26 +1,46 @@
 var express = require('express');
 var routes = express.Router();
 
+var names = ["Astral", "Scarlett", "Skarin"];
+
 var mijnObject = {
 	mijntekst: 'Hello world'
 };
 
-routes.post('/hello', function(req, res){
- var body = req.body;
- 
- console.dir(body);
- 
- res.contentType('application/json');
- res.status(200);
- res.json(mijnObject);
+routes.delete('/delete', function(req, res){
+	console.log(req.body);
+	var index = names.indexOf(req.body.name);
+	if(index > -1){
+		names.splice(index, 1);	
+	}
+	res.contentType('application/json');
+	res.status(200);
+	res.json({'Names' : names});
+});
+
+routes.post('/post', function(req, res){
+	
+	console.log(req.body);
+	var nameToAdd = req.body.name;
+	
+	names.push(nameToAdd);
+	res.contentType('application/json');
+	res.status(200);
+	res.json({'Names' : names});
 });
 
 
-routes.get('/goodbye', function(req, res){
+routes.get('/get', function(req, res){
 	
 	res.contentType('application/json');
 	res.status(200);
-	res.json({'tekst' : 'Goodbye'});
+	res.json({'Names' : names});
+});
+
+routes.put('/put', function(req, res){
+
+	
+
 });
 
 module.exports = routes;
